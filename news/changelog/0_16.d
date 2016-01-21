cd914db4470662e179c1afb1abd6254c29cbfee4
Ulya Trofimovich <skvadrik@gmail.com>
2016-01-21 11:58:12 +0000

    Release 0.16.

---
+++ news/changelog/0_16.d
@@ -0,0 +1,1 @@
+src/news/changelog/0_16.html: ../src/template.html ../src/css/default.css
---
+++ news/release_notes/0_16.d
@@ -0,0 +1,1 @@
+src/news/release_notes/0_16.html: ../src/template.html ../src/news/home.rst ../src/css/default.css
